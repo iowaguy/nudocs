a57318bdb19803e695e11a8ee4879f8429729881
Ben Weintraub <benweintraub34@gmail.com>
2018-11-11 14:33:35 -0500

    implement tcp dummy server

--- main.go
+++ main.go
@@ -1,44 +1,102 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"net/http"
+	"fmt"
+	"math/rand"
+	"net"
+	"os"
+	"strconv"
+	"time"
+)
 
-	"github.com/gorilla/mux"
+const (
+	connHost = "localhost"
+	connPort = "3333"
+	connType = "tcp"
 )
 
 func main() {
-	myRouter := mux.NewRouter()
-	myRouter.HandleFunc("/operations/generate", operationReceiver).Methods("POST")
-	myRouter.HandleFunc("/operations/receive", operationSender).Methods("GET")
-	http.Handle("/", myRouter)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
+	// Listen for incoming connections.
+	l, err := net.Listen(connType, connHost+":"+connPort)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	defer l.Close()
 
-func operationReceiver(w http.ResponseWriter, r *http.Request) {
-	var o Operation
+	fmt.Println("Listening on " + connHost + ":" + connPort)
+	for {
+		// Listen for an incoming connection.
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
 
-	if err := json.NewDecoder(r.Body).Decode(&o); err != nil {
-		log.Fatal(err)
+		go receiveOperations(conn)
+		go sendOperations(conn)
 	}
 
-	w.Write([]byte("{\"status\":\"operation received\"}"))
 }
 
-func operationSender(w http.ResponseWriter, r *http.Request) {
-	var o Operation
-	o.OpType = "insert"
-	o.Character = "x"
-	o.Position = 5
-	o.Priority = 1
+func generateRandomOperation() operation {
+	rand.Seed(time.Now().UTC().UnixNano())
+	var o operation
 
-	json.NewEncoder(w).Encode(o)
+	if rand.Intn(2) == 1 {
+		o.opType = "i"
+	} else {
+		o.opType = "d"
+	}
+
+	o.character = string(byte(rand.Intn(26) + 65))
+	o.position = rand.Intn(128)
+
+	return o
 }
 
-type Operation struct {
-	OpType    string `json:"opType"`
-	Character string `json:"character"`
-	Position  int    `json:"position,string"`
-	Priority  int    `json:"priority,string"`
+func sendOperations(conn net.Conn) {
+	defer conn.Close()
+
+	for {
+		o := generateRandomOperation()
+
+		conn.Write([]byte(o.String()))
+		time.Sleep(2 * time.Second)
+	}
+}
+
+func receiveOperations(conn net.Conn) {
+	defer conn.Close()
+	// Make a buffer to hold incoming data.
+	buf := make([]byte, 1024)
+
+	for {
+		// Read the incoming connection into the buffer.
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading:", err.Error())
+			break
+		}
+
+		var o operation
+		o.opType = string(buf[0])
+		o.character = string(buf[1])
+		if o.position, err = strconv.Atoi(string(buf[2 : n-1])); err != nil {
+			fmt.Println("Error: could not parse position int", err.Error())
+		}
+
+		// Send a response back to person contacting us.
+		conn.Write([]byte("ok"))
+	}
+}
+
+type operation struct {
+	opType    string
+	character string
+	position  int
+}
+
+func (o *operation) String() string {
+	return fmt.Sprintf(o.opType + o.character + strconv.Itoa(o.position) + "\n")
 }
