4aebf0e3a39d3864cdd13e8e7be8953ba91b4883
Ben Weintraub <benweintraub34@gmail.com>
2018-11-16 18:31:43 -0500

    finished reduce alg step 3

--- core/opt.go
+++ core/opt.go
@@ -8,7 +8,7 @@ import (
 )
 
 type reduce struct {
-	historyBuffer *list.List
+	historyBuffer []*common.PeerOperation
 	proposed      chan *common.PeerOperation
 	ready         chan *common.PeerOperation
 }
@@ -20,7 +20,7 @@ var onceRed sync.Once
 func NewReducer(peers, pid int) *reduce {
 	onceRed.Do(func() {
 		instantiatedReduce = &reduce{}
-		instantiatedReduce.historyBuffer = list.New()
+		instantiatedReduce.historyBuffer = make([]*common.PeerOperation, 1024)
 		instantiatedReduce.proposed = make(chan *common.PeerOperation, 100)
 		instantiatedReduce.ready = make(chan *common.PeerOperation, 10)
 
@@ -54,52 +54,96 @@ func (r *reduce) Ready() <-chan *common.PeerOperation {
 }
 
 func (r *reduce) Start() {
-	// pop op off proposed queue
-	o := <-r.proposed
+	for {
+		// pop op off proposed queue
+		o := <-r.proposed
 
-	var e *list.Element
+		var e *list.Element
 
-	// section 1 of REDUCE algorithm
-	// search for first operation that is independent of o in historyBuffer
-	for e = r.historyBuffer.Front(); e != nil; e = e.Next() {
-		po := e.Value.(common.PeerOperation)
-		if o.VClock.Independent(&po.VClock) {
-			break
-		}
-	}
-
-	if e == nil {
-		// put o in outgoing queue, o can be exectuted
-		r.ready <- o
-	}
-
-	// section 2 of REDUCE algorithm
-	// if im here, o is independent of e
-	// look for operations causally preceding o
-	k := *e
-	for e = k.Next(); e != nil; e = e.Next() {
-		po := e.Value.(common.PeerOperation)
-		if o.VClock.HappenedAfter(&po.VClock) {
-			break
-		}
-	}
-
-	if e == nil {
-		eo := o
-
-		// perform an inclusion trasformation on o against everything in the history buffer, in the language of the paper: EO := LIT(O, L[k,m])
-		for e = &k; e != nil; e = e.Next() {
-			po := e.Value.(common.PeerOperation)
-			eo = InclusionTransformation(eo, &po)
+		// section 1 of REDUCE algorithm
+		// search for first operation that is independent of o in historyBuffer
+		noOpsIndependent := true
+		var k int
+		for k, po := range r.historyBuffer {
+			if o.VClock.Independent(po.VClock) {
+				noOpsIndependent := false
+				break
+			}
 		}
 
+		if noOpsIndependent {
+			// put o in outgoing queue, o can be exectuted
+			r.ready <- o
+			continue
+		}
+
+		// section 2 of REDUCE algorithm
+		// if im here, o is independent of e
+		// look for operations causally preceding o
+		noOpsCausallyPreceding := true
+		for _, po := range r.historyBuffer[k+1:] {
+			if o.VClock.HappenedAfter(po.VClock) {
+				noOpsCausallyPreceding := false
+				break
+			}
+		}
+
+		if noOpsCausallyPreceding {
+			// perform an inclusion trasformation on o against everything in
+			// the history buffer, in the language of the paper:
+			// EO := LIT(O, L[k,m])
+			eo := LIT(o, r.historyBuffer[k:])
+			r.ready <- eo
+			continue
+		}
+
+		// if im here, then there is at least one operation which causally
+		// precedes o, but comes after an operation which is independent of o
+
+		// section 3 of REDUCE algorithm
+		// generate a list L1 which contains the operations in L[k,m] which are
+		// causally preceding o
+
+		// cs is a slice of the indexes of operations which are causally
+		// preceding o
+		cs = make([]int, len(r.historyBuffer))
+		l1 := make([]*common.PeerOperation, len(r.historyBuffer))
+		for i, po := range r.historyBuffer[k:] {
+			if o.VClock.HappenedAfter(po.VClock) {
+				append(cs, i)
+				append(l1, po)
+			}
+		}
+
+		// c1 is the first causally preceding operation following at least
+		// one independent operation of o
+		c1 := cs[0]
+		eoc1Prime := LET(l1[0], reverse(r.historyBuffer[k:c1-1]))
+		l1Prime := make([]*common.PeerOperation, len(r.historyBuffer))
+		append(l1Prime, eoc1Prime)
+		for _, eoci := range l1[1:] {
+			ci := cs[i-1]
+			ot := LET(eoci, reverse(r.historyBuffer[k:ci-1]))
+			eociPrime := LIT(ot, l1Prime)
+			append(l1Prime, eociPrime)
+		}
+
+		oPrime := LET(o, reverse(l1Prime))
+
+		eo := LIT(oPrime, r.historyBuffer[k:])
+		r.ready <- eo
 	}
-
-	// section 3 of REDUCE algorithm
-	// search for first operation that is independent of o in historyBuffer
-
 }
 
-func (r *reduce) log(o common.Operation) {
-	r.historyBuffer.PushBack(o)
+func (r *reduce) log(o *common.PeerOperation) {
+	append(r.historyBuffer, o)
+}
+
+func reverse(sl []*common.PeerOperation) []*common.PeerOperation {
+	rev := make([]*common.PeerOperation, len(sl))
+	for i := len(sl)/2 - 1; i >= 0; i-- {
+		opp := len(sl) - 1 - i
+		rev[i] = sl[opp]
+		// sl[i], sl[opp] = sl[opp], sl[i]
+	}
 }
--- core/transformations.go
+++ core/transformations.go
@@ -11,3 +11,19 @@ func ExclusionTransformation(o1, o2 *common.PeerOperation) *common.PeerOperation
 	// TODO needs to determine exactly which function in the transformation matrix to call
 	return nil
 }
+
+func LET(eo *common.PeerOperation, l []*common.PeerOperation) *common.PeerOperation {
+	for _, po := range l {
+		eo = ExclusionTransformation(eo, po)
+	}
+
+	return eo
+}
+
+func LIT(eo *common.PeerOperation, l []*common.PeerOperation) *common.PeerOperation {
+	for _, po := range l {
+		eo = InclusionTransformation(eo, po)
+	}
+
+	return eo
+}
