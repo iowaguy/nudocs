ea133c2d80e7c031da5fa1b993b20b52dc87bef5
Ben Weintraub <benweintraub34@gmail.com>
2018-11-17 17:37:13 -0500

    nudocs minor mode is showing up

--- nu-docs-mode.el
+++ nu-docs-mode.el
@@ -1,30 +1,50 @@
 
-;;;###autoload
-(define-minor-mode nu-docs-mode
-  :lighter "NUDOCS"
-
-  ;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Network-Processes.html
-  (setq pserv (make-network-process :name "nu-docs" :service 3333))
-
-  (defun nudocs-post-command-hook ()
-    (setq c "")
-    (cond ((= last-command-event 127) (setq c "d"))
-          ((= last-command-event 4) (setq c "d"))
-          ((> last-command-event 64) (setq c "i")))
-    (process-send-string pserv (format "%s%c%d" c last-command-event (point))))
-
-  (defun handle-server-reply (process content)
-    ;; get messages from server, execute commands it specifies
-
-    ;; parse command, i/d, char, position
-
-    (message content))
-
-  (add-hook 'post-command-hook 'nudocs-post-command-hook nil t)
-  (set-process-filter pserv 'handle-server-reply))
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Network-Processes.html
+;; (setq pserv (make-network-process :name "nu-docs" :service 3333))
 
 
-(provide 'nu-docs-mode)
+
+(defun nudoc-send-string (c)
+  (process-send-string pserv (format "%s%c%d" c last-command-event (point))))
+
+(defun nudocs-post-command-hook ()
+  (setq c "")
+  (cond ((= last-command-event 127) (nudoc-send-string "d"))
+        ((= last-command-event 4) (nudoc-send-string "d"))
+        ((> last-command-event 64) (nudoc-send-string "i"))))
+
+(defun handle-server-reply (process content)
+  ;; get messages from server, execute commands it specifies
+  ;; parse command, i/d, char, position
+  (message content))
+
+(defun nudocs-mode-enter ()
+  "Called when entering nudocs mode"
+  (progn
+    (make-variable-buffer-local
+     (defvar pserv (make-network-process :name "nudocs" :service 3333)))
+    (add-hook 'post-command-hook 'nudocs-post-command-hook nil t)
+    (set-process-filter pserv 'handle-server-reply)))
+
+(defun nudocs-mode-exit ()
+  (remove-hook 'post-command-hook 'nudocs-post-command-hook t))
+
+(define-minor-mode nudocs-mode
+  "Minor mode for using NUDOCS"
+  :lighter " nudocs"
+
+  (if nudocs-mode
+      (nudocs-mode-enter)
+    (nudocs-mode-exit)))
+
+
+  ;; (progn
+  ;;   (setup-nudoc-connections)
+  ;;   (add-hook 'post-command-hook 'nudocs-post-command-hook nil t)
+  ;;   (set-process-filter pserv 'handle-server-reply)))
+
+
+;; (provide 'nudocs-mode)
 
 
 
