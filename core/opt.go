608bbb215bb956bd577b9be518fd3d5e62c07b72
Ben Weintraub <benweintraub34@gmail.com>
2018-11-30 15:01:17 -0500

    Implement undo/do/redo for convergence guarantee

--- core/opt.go
+++ core/opt.go
@@ -11,7 +11,6 @@ import (
 )
 
 type OpTransformer interface {
-	Log(o *common.PeerOperation)
 	Ready() <-chan *common.Operation
 	Start()
 	PeerPropose(o *common.PeerOperation)
@@ -19,9 +18,12 @@ type OpTransformer interface {
 }
 
 type Reduce struct {
+	// these are the operations that have been executed
 	historyBuffer []*common.PeerOperation
-	proposed      chan *common.PeerOperation
-	ready         chan *common.Operation
+
+	// these are the causally ready operations
+	proposed chan *common.PeerOperation
+	ready    chan *common.Operation
 }
 
 // a singleton
@@ -41,7 +43,7 @@ func GetReducer() *Reduce {
 		// unaccounted for in operations waiting in the ready queue.
 		// a new operation can only be processed after the
 		// ready queue is emptied.
-		instantiatedReduce.ready = make(chan *common.Operation)
+		instantiatedReduce.ready = make(chan *common.Operation, 1024)
 	})
 	return instantiatedReduce
 }
@@ -73,86 +75,145 @@ func (r *Reduce) Ready() <-chan *common.Operation {
 
 func (r *Reduce) Start() {
 	for {
-		// pop op off proposed queue
-		o := <-r.proposed
+		// pop causally ready operation off proposed queue
+		oNew := <-r.proposed
 
-		// section 1 of REDUCE algorithm
-		// search for first operation that is independent of o in historyBuffer
-		noOpsIndependent := true
-		var k int
-		for i, po := range r.historyBuffer {
-			k = i
-			if o.VClock.Independent(&po.VClock) {
-				noOpsIndependent = false
+		// (1) Undo
+		var i int
+		for _, eo := range reverse(r.historyBuffer) {
+			if eo == nil {
 				break
 			}
-		}
 
-		if noOpsIndependent {
-			// put o in outgoing queue, o can be exectuted
-			r.ready <- &o.Operation
-			continue
-		}
-
-		// section 2 of REDUCE algorithm
-		// if im here, o is independent of e
-		// look for operations causally preceding o
-		noOpsCausallyPreceding := true
-		for _, po := range r.historyBuffer[k+1:] {
-			if o.VClock.HappenedAfter(&po.VClock) {
-				noOpsCausallyPreceding = false
+			if eo.VClock.HappenedBefore(&oNew.VClock) {
 				break
 			}
+
+			//   write the undo of each operation in HB to ready
+			r.ready <- common.UndoOperation(eo)
 		}
 
-		if noOpsCausallyPreceding {
-			// perform an inclusion trasformation on o against everything in
-			// the history buffer, in the language of the paper:
-			// EO := LIT(O, L[k,m])
-			eo := LIT(o, r.historyBuffer[k:])
-			r.ready <- &eo.Operation
-			continue
+		undone := make([]*common.PeerOperation, 1024)
+		lastPrecedingOpIndex := len(r.historyBuffer) - i - 1
+
+		copy(undone, r.historyBuffer[lastPrecedingOpIndex+1:])
+
+		// remove everything after (and including) newI + 1 from
+		// history. need to do newI + 1, because we don't want to
+		// include the operation that happened before oNew
+		r.historyBuffer = r.historyBuffer[:lastPrecedingOpIndex+1]
+
+		// (2) Transform Do
+		eoNew := r.got(oNew)
+
+		// (3) Transform Redo
+		eom1Prime := InclusionTransformation(undone[0], eoNew)
+		undone = undone[1:]
+
+		transformedRedos := make([]*common.PeerOperation, 0, 1024)
+		transformedRedos = append(transformedRedos, eoNew, eom1Prime)
+		for i, eomi := range undone {
+			// (3.1)
+			to := LET(eomi, reverse(undone[:i]))
+
+			// (3.2)
+			eomiPrime := LIT(to, transformedRedos)
+			transformedRedos = append(transformedRedos, eomiPrime)
+
 		}
 
-		// if im here, then there is at least one operation which causally
-		// precedes o, but comes after an operation which is independent of o
-
-		// section 3 of REDUCE algorithm
-		// generate a list L1 which contains the operations in L[k,m] which are
-		// causally preceding o
-
-		// cs is a slice of the indexes of operations which are causally
-		// preceding o
-		cs := make([]int, len(r.historyBuffer))
-		l1 := make([]*common.PeerOperation, len(r.historyBuffer))
-		for i, po := range r.historyBuffer[k:] {
-			if o.VClock.HappenedAfter(&po.VClock) {
-				cs = append(cs, i)
-				l1 = append(l1, po)
+		// write transformed ops to ready
+		for _, op := range transformedRedos {
+			if op == nil {
+				break
 			}
+			r.log(op)
+			r.ready <- &op.Operation
 		}
-
-		// c1 is the first causally preceding operation following at least
-		// one independent operation of o
-		c1 := cs[0]
-		eoc1Prime := LET(l1[0], reverse(r.historyBuffer[k:c1-1]))
-		l1Prime := make([]*common.PeerOperation, len(r.historyBuffer))
-		l1Prime = append(l1Prime, eoc1Prime)
-		for i, eoci := range l1[1:] {
-			ci := cs[i-1]
-			ot := LET(eoci, reverse(r.historyBuffer[k:ci-1]))
-			eociPrime := LIT(ot, l1Prime)
-			l1Prime = append(l1Prime, eociPrime)
-		}
-
-		oPrime := LET(o, reverse(l1Prime))
-
-		eo := LIT(oPrime, r.historyBuffer[k:])
-		r.ready <- &eo.Operation
 	}
 }
 
-func (r *Reduce) Log(o *common.PeerOperation) {
+// This is the Generic Operational Transformation algorithm
+func (r *Reduce) got(o *common.PeerOperation) *common.PeerOperation {
+	// section 1 of REDUCE algorithm
+	// search for first operation that is independent of o in historyBuffer
+	noOpsIndependent := true
+	var k int
+	for i, po := range r.historyBuffer {
+		k = i
+		if o.VClock.Independent(&po.VClock) {
+			noOpsIndependent = false
+			break
+		}
+	}
+
+	if noOpsIndependent {
+		// put o in outgoing queue, o can be exectuted
+		return o
+		// r.ready <- &o.Operation
+		// continue
+	}
+
+	// section 2 of REDUCE algorithm
+	// if im here, o is independent of e
+	// look for operations causally preceding o
+	noOpsCausallyPreceding := true
+	for _, po := range r.historyBuffer[k+1:] {
+		if o.VClock.HappenedAfter(&po.VClock) {
+			noOpsCausallyPreceding = false
+			break
+		}
+	}
+
+	if noOpsCausallyPreceding {
+		// perform an inclusion trasformation on o against everything in
+		// the history buffer, in the language of the paper:
+		// EO := LIT(O, L[k,m])
+		eo := LIT(o, r.historyBuffer[k:])
+		return eo
+		// r.ready <- &eo.Operation
+		// continue
+	}
+
+	// if im here, then there is at least one operation which causally
+	// precedes o, but comes after an operation which is independent of o
+
+	// section 3 of REDUCE algorithm
+	// generate a list L1 which contains the operations in L[k,m] which are
+	// causally preceding o
+
+	// cs is a slice of the indexes of operations which are causally
+	// preceding o
+	cs := make([]int, len(r.historyBuffer))
+	l1 := make([]*common.PeerOperation, len(r.historyBuffer))
+	for i, po := range r.historyBuffer[k:] {
+		if o.VClock.HappenedAfter(&po.VClock) {
+			cs = append(cs, i)
+			l1 = append(l1, po)
+		}
+	}
+
+	// c1 is the first causally preceding operation following at least
+	// one independent operation of o
+	c1 := cs[0]
+	eoc1Prime := LET(l1[0], reverse(r.historyBuffer[k:c1-1]))
+	l1Prime := make([]*common.PeerOperation, len(r.historyBuffer))
+	l1Prime = append(l1Prime, eoc1Prime)
+	for i, eoci := range l1[1:] {
+		ci := cs[i-1]
+		ot := LET(eoci, reverse(r.historyBuffer[k:ci-1]))
+		eociPrime := LIT(ot, l1Prime)
+		l1Prime = append(l1Prime, eociPrime)
+	}
+
+	oPrime := LET(o, reverse(l1Prime))
+
+	eo := LIT(oPrime, r.historyBuffer[k:])
+	// r.ready <- &eo.Operation
+	return eo
+}
+
+func (r *Reduce) log(o *common.PeerOperation) {
 	r.historyBuffer = append(r.historyBuffer, o)
 }
 
