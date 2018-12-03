22d4a32a0ddda937f9928138ec3fcf1beb4cd953
Ben Weintraub <benweintraub34@gmail.com>
2018-12-02 22:45:25 -0500

    Emacs client is splitting docs correctly

--- client/nu-docs-mode.el
+++ client/nu-docs-mode.el
@@ -1,6 +1,7 @@
 
 
 (defun nudocs-send-raw (pserv s)
+;;  (message s)
   (process-send-string pserv s))
 
 (defun nudocs-send-operation (pserv c)
@@ -11,10 +12,33 @@
         ((= last-command-event 4) (nudocs-send-operation pserv "d"))
         ((> last-command-event 64) (nudocs-send-operation pserv "i"))))
 
+(defun nudocs-set-buffer (content)
+  (progn
+    (setq curr-point (point))
+    ;; clear old content from buffer
+    (erase-buffer)
+
+    ;; write new doc to buffer
+    (insert content)
+    (goto-char curr-point)))
+
+;; returns a list of valid doc strings
+(defun nudocs-split-string (original accumulator)
+  (progn
+    (setq lengthContent (car (split-string original ":" t)))
+    (if lengthContent
+        accumulator
+      (progn
+        (setq length (string-to-number (car l)))
+
+        (setq accumulator (add-to-list accumulator (substring (cdr l) 0 length)))
+
+        ;; need to remove
+        (nudocs-split-string (substring (cdr l) length nil))))))
+
 (defun handle-server-reply (process content)
-  ;; get messages from server, execute commands it specifies
-  ;; parse command, i/d, char, position
-  (message content))
+  ;; split content at demarcation character
+  (mapc '(nudocs-set-buffer content) (nudocs-split-string content '())))
 
 (defun nudocs-mode-enter ()
   "Called when entering nudocs mode"
@@ -27,7 +51,7 @@
 
     ;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Network-Processes.html
     (setq pserv (make-network-process :name "nudocs" :service 3333))
-    (set (make-local-variable 'peer-server) pserv)
+    (set-process-filter pserv 'handle-server-reply)
     (nudocs-send-raw pserv "client")
     (add-hook 'post-command-hook (lambda () (nudocs-post-command-hook pserv)) nil t)))
 
